941b3bf75f9fd0a863301fd33c8b04e1d28ad26e
shubcape <shubcape@21-school.ru>
2025-12-17 00:48:30 +0500

    10 lik sanoq tizimidan 16 lik sanoq tizimiga o'tkazuvchi dastur tayyor

--- sanoq_sistemalar.py
+++ sanoq_sistemalar.py
@@ -1,16 +1,18 @@
-def decimal_to_octal(n):
+def decimal_to_hexadecimal(n):
     if n == 0:
         return "0"
 
-    octal = ""
-    while n > 0:
-        qoldiq = n % 8
-        octal = str(qoldiq) + octal
-        n //= 8
+    belgilar = "0123456789ABCDEF"
+    hex_son = ""
 
-    return octal
+    while n > 0:
+        qoldiq = n % 16
+        hex_son = belgilar[qoldiq] + hex_son
+        n //= 16
+
+    return hex_son
 
 
 son = int(input("10-lik son kiriting: "))
-natija = decimal_to_octal(son)
-print("8-lik sanoq sistemasida:", natija)
+natija = decimal_to_hexadecimal(son)
+print("16-lik sanoq sistemasida:", natija)
