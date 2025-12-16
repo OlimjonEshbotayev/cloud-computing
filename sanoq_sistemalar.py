d52311235623cee7a4bf249674963b6a964a7e87
shubcape <shubcape@21-school.ru>
2025-12-17 00:47:51 +0500

    10 lik sanoq tizimidan 8 lik sanoq tizimiga o'tkazuvchi dastur tayyor

--- sanoq_sistemalar.py
+++ sanoq_sistemalar.py
@@ -1,16 +1,16 @@
-def decimal_to_binary(n):
+def decimal_to_octal(n):
     if n == 0:
         return "0"
 
-    binary = ""
+    octal = ""
     while n > 0:
-        qoldiq = n % 2
-        binary = str(qoldiq) + binary
-        n //= 2
+        qoldiq = n % 8
+        octal = str(qoldiq) + octal
+        n //= 8
 
-    return binary
+    return octal
 
 
 son = int(input("10-lik son kiriting: "))
-natija = decimal_to_binary(son)
-print("2-lik sanoq sistemasida:", natija)
+natija = decimal_to_octal(son)
+print("8-lik sanoq sistemasida:", natija)
