a2b8bfae0ce8424591c73985c78c7def635982a1
shubcape <shubcape@21-school.ru>
2025-12-17 09:37:16 +0500

    8 lik tizimga otkazsih qoshilgan kod

--- new.py
+++ new.py
@@ -14,3 +14,20 @@ def decimal_to_binary(n):
 son = int(input("10-lik son kiriting: "))
 natija = decimal_to_binary(son)
 print("2-lik sanoq sistemasida:", natija)
+
+def decimal_to_octal(n):
+    if n == 0:
+        return "0"
+
+    octal = ""
+    while n > 0:
+        qoldiq = n % 8
+        octal = str(qoldiq) + octal
+        n //= 8
+
+    return octal
+
+
+son = int(input("10-lik son kiriting: "))
+natija = decimal_to_octal(son)
+print("8-lik sanoq sistemasida:", natija)
