7bba5561e015a6c467c9aaafc847ff875d64e117
shubcape <shubcape@21-school.ru>
2025-12-17 09:38:10 +0500

    16 lik tizimga otkazsih qoshilgan kod

--- new.py
+++ new.py
@@ -31,3 +31,22 @@ def decimal_to_octal(n):
 son = int(input("10-lik son kiriting: "))
 natija = decimal_to_octal(son)
 print("8-lik sanoq sistemasida:", natija)
+
+def decimal_to_hexadecimal(n):
+    if n == 0:
+        return "0"
+
+    belgilar = "0123456789ABCDEF"
+    hex_son = ""
+
+    while n > 0:
+        qoldiq = n % 16
+        hex_son = belgilar[qoldiq] + hex_son
+        n //= 16
+
+    return hex_son
+
+
+son = int(input("10-lik son kiriting: "))
+natija = decimal_to_hexadecimal(son)
+print("16-lik sanoq sistemasida:", natija)
